78e405b635d2ef2bcb2cb1749dae77666d48e382
BigFatLuu <bigfatluu@gmail.com>
2017-02-15 23:25:45 +0100

    the size of the largest possible subset (S')

---
+++ src/algorithms/implementation/NonDivisibleSubset.java
@@ -0,0 +1,26 @@
+package algorithms.implementation;
+
+import java.util.Scanner;
+
+public class NonDivisibleSubset {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] table = new int [k];
+        int subset = 0;
+        for (int i = 0; i < n; i++){
+            int ai = sc.nextInt()%k;
+            table[ai]++;
+        }
+        for(int i = 0; i <= k/2; i++){
+            int r = (k-i)%k;
+            if (i==r){
+                subset += Math.min(table[i],1);
+            }else {
+                subset += Math.max(table[i], table[r]);
+            } 
+        }
+        System.out.print(subset);
+    }
+}
